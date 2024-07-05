dddb226879bfd7e94a115ccb4316d47e8b211088
Iván Cea Fontenla <ivancea96@outlook.com>
2024-07-05 20:13:21 +0200

    Updated to eslint 9 and fixed configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,18 @@
 // @ts-check
 
+import { includeIgnoreFile } from "@eslint/compat";
 import eslint from "@eslint/js";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import path from "path";
 import tseslint from "typescript-eslint";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default tseslint.config(
+  includeIgnoreFile(gitignorePath),
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   eslintPluginPrettierRecommended,
