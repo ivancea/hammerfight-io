7374d365fc5377dcef0c96f40c7efa2406aa7126
Iván Cea Fontenla <ivancea96@outlook.com>
2024-07-22 20:47:34 +0200

    Added git commit and date to frontend

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+import childProcess from "child_process";
 import DotenvWebpackPlugin from "dotenv-webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
@@ -7,48 +8,69 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default {
-  entry: "./client/index.ts",
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        include: [
-          path.resolve(__dirname, "client"),
-          path.resolve(__dirname, "common"),
-        ],
-      },
-      {
-        test: /\.s?css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-        include: path.resolve(__dirname, "client/styles"),
-      },
-      {
-        test: /\.(jpg|png)$/,
-        include: path.resolve(__dirname, "client/assets"),
-        type: "asset/resource",
-      },
+export default (env) => {
+  let appVersion;
+
+  if (env.APP_VERSION) {
+    appVersion = env.APP_VERSION;
+  } else {
+    const gitCommit = childProcess.execSync("git rev-parse HEAD").toString();
+    const commitDate = new Date(
+      Number(
+        childProcess
+          .execSync("git show -s --format=%ct HEAD")
+          .toString()
+          .trim(),
+      ) * 1000,
+    );
+
+    appVersion = `${gitCommit} - ${commitDate.toISOString()}`;
+  }
+
+  return {
+    entry: "./client/index.ts",
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: "ts-loader",
+          include: [
+            path.resolve(__dirname, "client"),
+            path.resolve(__dirname, "common"),
+          ],
+        },
+        {
+          test: /\.s?css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+          include: path.resolve(__dirname, "client/styles"),
+        },
+        {
+          test: /\.(jpg|png)$/,
+          include: path.resolve(__dirname, "client/assets"),
+          type: "asset/resource",
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+      filename: "bundle.js",
+      assetModuleFilename: "assets/[hash].[name][ext][query]",
+      path: path.resolve(__dirname, "build"),
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./client/index.html",
+        appVersion,
+      }),
+      new MiniCssExtractPlugin(),
+      new DotenvWebpackPlugin({
+        safe: ".env.defaults",
+        defaults: ".env.defaults",
+        systemvars: true,
+      }),
     ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  output: {
-    filename: "bundle.js",
-    assetModuleFilename: "assets/[hash].[name][ext][query]",
-    path: path.resolve(__dirname, "build"),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./client/index.html",
-    }),
-    new MiniCssExtractPlugin(),
-    new DotenvWebpackPlugin({
-      safe: ".env.defaults",
-      defaults: ".env.defaults",
-      systemvars: true,
-    }),
-  ],
+  };
 };
