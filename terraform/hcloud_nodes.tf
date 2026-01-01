4a7285e393ea8b2a123f7909c491ae236b18d234
Iván Cea Fontenla <ivancea96@outlook.com>
2026-01-01 18:27:34 +0100

    Use Hetzner cx23 server

--- terraform/hcloud_nodes.tf
+++ terraform/hcloud_nodes.tf
@@ -2,7 +2,7 @@ resource "hcloud_server" "node-eu" {
   name        = "${var.node_name_prefix}-0"
   location    = "fsn1"
   image       = "debian-12"
-  server_type = "cx22"
+  server_type = "cx23"
   user_data = templatefile("initialize-node.sh",
     {
       git_commit              = data.git_commit.current_commit.sha1,
